9b0a466c1d30e4e21192b4889376c88bc5f16d62
pshatara <pshatara@gmail.com>
2014-09-18 16:01:58 -0700

    Complete solution for nQueens.

--- src/solvers.js
+++ src/solvers.js
@@ -53,7 +53,11 @@ window.countNRooksSolutions = function(n) {
       if (position !== n-1) {
         if (testArr[position][i - 1] === 1) {
           for (var j = position; j < n; j++) {
-            testArr[j][i-1] = 0;
+            if (testArr[j][i-1] === 1) {
+              testArr[j][i-1] -= 1;
+            } else {
+              testArr[j][i-1] -=2;
+            }
           }
         }
         // If the current node is open
@@ -61,7 +65,7 @@ window.countNRooksSolutions = function(n) {
           testArr[position][i] = 1;
           position++;
           for (var j = position; j < n; j++) {
-            testArr[j][i] = 'X';
+            testArr[j][i] += 2;
           }
           recurse();
         }
@@ -75,7 +79,11 @@ window.countNRooksSolutions = function(n) {
     if (position !== 0) {
       if (testArr[position][n-1] === 1) {
         for (var i = position; i < n; i++) {
-          testArr[i][n-1] = 0;
+          if (testArr[i][n-1] === 1) {
+            testArr[i][n-1] -= 1;
+          } else {
+            testArr[i][n-1] -= 2;
+          }
         }
       }
       position--;
@@ -102,7 +110,85 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var position = 0;
+  var testArr = [];
+
+  for (var i = 0; i < n; i++) {
+    testArr.push([]);
+    for (var j = 0; j < n; j++) {
+      testArr[i].push(0)
+    }
+  }
+
+  var recurse = function() {
+    for (var i = 0; i < n; i++) {
+
+      // If not on the last row
+      if (position !== n-1) {
+        if (testArr[position][i - 1] === 1) {
+          for (var j = position, diag = 0; j < n; j++) {
+            if (testArr[j][i-1] === 1) {
+              testArr[j][i-1] -= 1;
+            } else {
+              testArr[j][i-1] -=2;
+              if (i-1-diag >= 0) {
+                testArr[j][i-1-diag] -= 2;
+              }
+              if (i < n) {
+                testArr[j][i-1+diag] -= 2;
+              }
+            }
+            diag++;
+          }
+        }
+        // If the current node is open
+        if (testArr[position][i] === 0) {
+          testArr[position][i] = 1;
+          position++;
+          for (var j = position, diag = 1; j < n; j++) {
+            testArr[j][i] += 2;
+            if (i-diag >= 0) {
+              testArr[j][i-diag] += 2;
+            }
+            if (i+diag < n) {
+              testArr[j][i+diag] += 2;
+            }
+            diag++;
+          }
+          recurse();
+        }
+
+      } else if (testArr[position][i] === 0) {
+        solutionCount++;
+      }
+
+    }
+
+    if (position !== 0) {
+      if (testArr[position][n-1] === 1) {
+        for (var i = position, diag = 0; i < n; i++) {
+          if (testArr[i][n-1] === 1) {
+            testArr[i][n-1] -= 1;
+          } else {
+            testArr[i][n-1] -= 2;
+            if (n-1-diag >= 0) {
+              testArr[i][n-1-diag] -= 2;
+            }
+          }
+          diag++;
+        }
+      }
+      position--;
+    }
+  };
+
+  // Accounting for trivial solutions & running recursive function
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    recurse();
+  }
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
